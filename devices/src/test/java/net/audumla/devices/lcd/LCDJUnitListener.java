f5fa4f78418cf76f434ebdd9c1df4ae37bd3e26c
audumla <audumla@31ee6a52-772c-232c-5b59-e78ef37cb892>
2014-01-23 13:07:50 +0000

    fixes for rpi

--- devices/src/test/java/net/audumla/devices/lcd/LCDJUnitListener.java
+++ devices/src/test/java/net/audumla/devices/lcd/LCDJUnitListener.java
@@ -18,6 +18,13 @@ public class LCDJUnitListener extends RunListener {
         if (target.initialize()) {
             logger.debug("Loaded JUnit RPII2CLCD Listener");
             scheduler.registerEventTarget(target);
+            try {
+                scheduler.publishEvent(target.getName(),
+                        new LCDClearCommand(),
+                        new LCDWriteCommand("LCD Started")).begin();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else {
             scheduler.unregisterEventTarget(target);
         }
