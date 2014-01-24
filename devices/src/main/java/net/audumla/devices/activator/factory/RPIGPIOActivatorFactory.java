f96648a05bc4569b50881b46f0f280515574de49
audumla <audumla@31ee6a52-772c-232c-5b59-e78ef37cb892>
2014-01-24 10:26:55 +0000

    RaspberryPI Testing

--- devices/src/main/java/net/audumla/devices/activator/factory/RPIGPIOActivatorFactory.java
+++ devices/src/main/java/net/audumla/devices/activator/factory/RPIGPIOActivatorFactory.java
@@ -18,7 +18,10 @@ package net.audumla.devices.activator.factory;
 
 import com.pi4j.io.gpio.PinEdge;
 import com.pi4j.wiringpi.Gpio;
-import net.audumla.devices.activator.*;
+import net.audumla.devices.activator.ActivatorCommand;
+import net.audumla.devices.activator.ActivatorState;
+import net.audumla.devices.activator.EventTransactionActivator;
+import net.audumla.devices.activator.EventTransactionActivatorFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,10 +33,14 @@ public class RPIGPIOActivatorFactory extends EventTransactionActivatorFactory<RP
     private static final Logger logger = LoggerFactory.getLogger(RPIGPIOActivatorFactory.class);
 
     protected ArrayList<RPIGPIOActivator> activators = new ArrayList<>();
+
     // the pin names and assignments are based on
     // http://wiringpi.com/wp-content/uploads/2013/03/gpio1.png
     // http://wiringpi.com/wp-content/uploads/2013/03/gpio21.png
-    public enum GPIOName {I2C_SDA, I2C_SCL, GPIO7, SPI_CE1, SPI_CE0, SPI_MISO, SPI_MOSI, SPI_SCLK, TxD, RxD, GPIO0, GPIO1, GPIO2, GPIO3, GPIO4, GPIO5, GPIO6, GPIO8, GPIO9, GPIO10, GPIO11}
+    public enum GPIOName {
+        I2C_SDA, I2C_SCL, GPIO7, SPI_CE1, SPI_CE0, SPI_MISO, SPI_MOSI, SPI_SCLK, TxD, RxD, GPIO0, GPIO1, GPIO2, GPIO3, GPIO4, GPIO5, GPIO6, GPIO8, GPIO9, GPIO10, GPIO11
+    }
+
     protected static int[][] GPIOPinRevisions = {
             {0, 1, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 23, 24, 25},
             {2, 3, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 27, 22, 23, 24, 25, 28, 29, 30, 31}};
@@ -53,9 +60,12 @@ public class RPIGPIOActivatorFactory extends EventTransactionActivatorFactory<RP
             for (int i = 0; i < GPIOPinRevisions[revisionIndex].length; ++i) {
                 logger.debug("Registering RaspberryPI pin [" + GPIOPinRevisions[revisionIndex][i] + ":" + GPIOName.values()[i].name() + "]");
                 RPIGPIOActivator a = new RPIGPIOActivator(GPIOPinRevisions[revisionIndex][i], GPIOName.values()[i], this);
-                a.allowSetState(true);
-                a.allowVariableState(false);
-                a.setState(ActivatorState.DEACTIVATED);
+                if (a.getName().startsWith("GPIO")) {
+                    //set all the GPIO pins to output and disasble.
+                    a.allowSetState(true);
+                    a.allowVariableState(false);
+                    a.setState(ActivatorState.DEACTIVATED);
+                }
                 activators.add(a);
             }
 
@@ -144,8 +154,7 @@ public class RPIGPIOActivatorFactory extends EventTransactionActivatorFactory<RP
             if (canSetState()) {
                 if (hasVariableState()) {
                     Gpio.pinMode(pin, Gpio.PWM_OUTPUT);
-                }
-                else {
+                } else {
                     Gpio.pinMode(pin, Gpio.OUTPUT);
                 }
             } else {
