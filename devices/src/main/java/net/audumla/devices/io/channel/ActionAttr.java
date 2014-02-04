7685ea79eb871eef7341f4ae4f5cf95b15eb007e
audumla <audumla@31ee6a52-772c-232c-5b59-e78ef37cb892>
2014-02-04 23:16:29 +0000

    Created Rpi io native library

--- devices/src/main/java/net/audumla/devices/io/channel/ActionAttr.java
+++ devices/src/main/java/net/audumla/devices/io/channel/ActionAttr.java
@@ -16,6 +16,6 @@ package net.audumla.devices.io.channel;
  *  See the License for the specific language governing permissions and limitations under the License.
  */
 
-public interface WaitAttr extends DeviceChannel.Attribute {
-    void executeWait();
+public interface ActionAttr extends DeviceChannel.Attribute {
+    void performAction();
 }
