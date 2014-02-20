9765bdf04719becaec2017c259d296fad900d211
audumla <audumla@31ee6a52-772c-232c-5b59-e78ef37cb892>
2014-02-20 07:51:12 +0000

    RPi I2C implementation

--- trunk/rpiio/src/main/native/net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative.c
+++ trunk/rpiio/src/main/native/net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative.c
@@ -8,6 +8,32 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include "net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative.h"
+
+// I2C definitions
+
+#define I2C_SLAVE	0x0703
+#define I2C_SMBUS	0x0720	/* SMBus-level access */
+
+#define I2C_SMBUS_READ	1
+#define I2C_SMBUS_WRITE	0
+
+// SMBus transaction types
+
+#define I2C_SMBUS_QUICK		    0
+#define I2C_SMBUS_BYTE		    1
+#define I2C_SMBUS_BYTE_DATA	    2
+#define I2C_SMBUS_WORD_DATA	    3
+#define I2C_SMBUS_PROC_CALL	    4
+#define I2C_SMBUS_BLOCK_DATA	    5
+#define I2C_SMBUS_I2C_BLOCK_BROKEN  6
+#define I2C_SMBUS_BLOCK_PROC_CALL   7		/* SMBus 2.0 */
+#define I2C_SMBUS_I2C_BLOCK_DATA    8
+
+// SMBus messages
+
+#define I2C_SMBUS_BLOCK_MAX	32	/* As specified in SMBus standard */
+#define I2C_SMBUS_I2C_BLOCK_MAX	32	/* Not specified but we use same structure */
+
 /*
  * Class:     net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative
  * Method:    open
@@ -71,6 +97,7 @@ JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative
  */
 JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_write__IIIII_3B_3B
   (JNIEnv *env, jclass clazz, jint fd, jint localAddress, jint offset, jint width, jint readCount, jbyteArray data, jbyteArray mask) {
+  return 0;
 };
 
 /*
@@ -80,6 +107,7 @@ JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative
  */
 JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_read__III_3BB
   (JNIEnv *env, jclass clazz, jint fd, jint offset, jint readCount, jbyteArray data, jbyte mask) {
+  return 0;
 };
 
 /*
@@ -89,5 +117,6 @@ JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative
  */
 JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_read__IIIII_3B_3B
   (JNIEnv *env, jclass clazz, jint fd, jint localAddress, jint offset, jint width, jint readCount, jbyteArray data, jbyteArray mask) {
+  return 0;
 };
 
