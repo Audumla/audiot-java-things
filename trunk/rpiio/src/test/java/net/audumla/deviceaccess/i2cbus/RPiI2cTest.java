e231bad3bb993967b186ef7fed802d9c7494c9bb
audumla <audumla@31ee6a52-772c-232c-5b59-e78ef37cb892>
2014-02-20 00:56:29 +0000

    RPi I2C implementation

--- trunk/rpiio/src/test/java/net/audumla/deviceaccess/i2cbus/RPiI2cTest.java
+++ trunk/rpiio/src/test/java/net/audumla/deviceaccess/i2cbus/RPiI2cTest.java
@@ -18,18 +18,64 @@ package net.audumla.deviceaccess.i2cbus;
 
 import net.audumla.deviceaccess.PeripheralManager;
 import net.audumla.deviceaccess.i2cbus.rpi.RPiI2CPeripheralProvider;
+import net.audumla.devices.activator.Activator;
+import net.audumla.devices.activator.ActivatorState;
+import net.audumla.devices.activator.AggregateActivator;
+import net.audumla.devices.activator.factory.PCF8574GPIOActivatorFactory;
+import net.audumla.devices.activator.factory.RPIGPIOActivatorFactory;
+import net.audumla.devices.io.channel.ChannelAddressAttr;
+import net.audumla.devices.io.channel.DeviceAddressAttr;
+import net.audumla.devices.io.channel.DeviceChannel;
+import net.audumla.devices.io.channel.I2CDeviceChannel;
+import net.audumla.devices.io.i2c.RPiI2CDeviceFactory;
+import net.audumla.devices.io.i2c.jni.rpi.I2C;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.ByteBuffer;
+import java.util.Properties;
+
 public class RPiI2cTest {
     private static final Logger logger = LoggerFactory.getLogger(RPiI2cTest.class);
+
+    static PCF8574GPIOActivatorFactory power5v;
+    static RPIGPIOActivatorFactory rpi = new RPIGPIOActivatorFactory();
+
+    static {
+        try {
+            rpi.initialize();
+        } catch (Exception e) {
+            logger.error("Cannot initialize PI", e);
+        }
+    }
+
+    static {
+        try {
+            DeviceChannel d = new I2CDeviceChannel(new RPiI2CDeviceFactory()).createChannel(new ChannelAddressAttr(1), new DeviceAddressAttr(0x27));
+            power5v = new PCF8574GPIOActivatorFactory(d);
+            power5v.initialize();
+        } catch (Exception e) {
+            logger.error("Failed to initialize power5v", e);
+        }
+    }
+
+    Activator getPower(int pin1_5v, int pin2_5v, Activator pin3_33v) throws Exception {
+        AggregateActivator aa = new AggregateActivator();
+        Properties id = new Properties();
+        id.setProperty(PCF8574GPIOActivatorFactory.PCF8547GPIOActivator.GPIO_PIN, String.valueOf(pin1_5v));
+        aa.addActivator(power5v.getActivator(id));
+        id.setProperty(PCF8574GPIOActivatorFactory.PCF8547GPIOActivator.GPIO_PIN, String.valueOf(pin2_5v));
+        aa.addActivator(power5v.getActivator(id));
+        aa.addActivator(pin3_33v);
+        return aa;
+    }
 
     public I2CDevice createI2CDevice() throws Exception {
         I2CDeviceConfig c = new I2CDeviceConfig(1, 0x21);
         RPiI2CPeripheralProvider provider = new RPiI2CPeripheralProvider();
-        I2CDevice device = provider.open(c,null, PeripheralManager.SHARED);
+        I2CDevice device = provider.open(c, null, PeripheralManager.SHARED);
         assert device != null;
         return device;
     }
@@ -55,5 +101,54 @@ public class RPiI2cTest {
         d.setMask(0xff);
         d.write((byte) ~val);
         Assert.assertEquals(~val & 0x0f, d.read());
+    }
+
+    @Test
+    public void testSainsSmartRelayFromPCF8574Direct() throws Exception {
+        synchronized (this) {
+            Activator power = getPower(6, 7, rpi.getActivator(RPIGPIOActivatorFactory.GPIOName.GPIO1));
+            I2CDevice d = createI2CDevice();
+            int repeat = 10;
+            logger.debug("Speed test 5ms");
+            wait(20);
+            d.write(0xff);
+            wait(20);
+            power.setState(ActivatorState.ACTIVATED);
+            byte[] bytes = new byte[8 * repeat];
+            for (int c = 0; c < repeat; ++c) {
+                byte val = (byte) 0x01;
+                for (int i = 0; i < 8; ++i) {
+                    bytes[(c * 8) + i] = (byte) ~val;
+                    d.write((byte) ~val);
+                    wait(5);
+                    val = (byte) (val << 1);
+                }
+            }
+            logger.debug("Speed test 2ms");
+            wait(1000);
+            d.write((byte) 0xff);
+            wait(20);
+            for (int c = 0; c < repeat; ++c) {
+                byte val = (byte) 0x01;
+                for (int i = 0; i < 8; ++i) {
+                    d.write((byte) ~val);
+                    wait(2);
+                    val = (byte) (val << 1);
+                }
+            }
+
+            wait(1000);
+            d.write((byte) 0xff);
+            wait(20);
+            logger.debug("Speed test 0ms");
+            ByteBuffer b = ByteBuffer.wrap(bytes);
+            for (int i = 0; i < repeat*100; ++i) {
+                int v = d.write(b);
+            }
+            d.write((byte) 0xff);
+            power.setState(ActivatorState.DEACTIVATED);
+            logger.debug("Finshed Speed test");
+
+        }
     }
 }
