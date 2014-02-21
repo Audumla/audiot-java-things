6d2f1285f18a6bf4d4870ac7ded77e71f8a14180
audumla <audumla@31ee6a52-772c-232c-5b59-e78ef37cb892>
2014-02-21 10:40:12 +0000

    RPi I2C implementation

--- rpiio/src/main/java/net/audumla/deviceaccess/i2cbus/rpi/jni/RPiI2CNative.java
+++ rpiio/src/main/java/net/audumla/deviceaccess/i2cbus/rpi/jni/RPiI2CNative.java
@@ -93,13 +93,12 @@ public class RPiI2CNative {
      * @param fd          file descriptor of i2c bus
      * @param address     the address of the device on the i2c bus
      * @param offset      the offset within the buffer to start the operation. The actual byte offset will be evaluated as offset * width
-     * @param width       the width in bytes for each read or write of the I2c device
      * @param writeBuffer an array to receive the read bytes of size width*readCount
      * @param writeCount  the number of times to read 'width' bytes from the device
      * @param mask        a bit mask that will be applied to every written to the device
      * @return return a negative value if an error was encountered otherwise the number of bytes read
      */
-    public static native int write(int fd, int address, int offset, int width, int writeCount, byte[] writeBuffer, byte[] mask);
+    public static native int write(int fd, int address, int offset, int writeCount, byte[] writeBuffer, byte mask);
 
     /**
      * Writes bytes to the i2c device.
@@ -137,12 +136,11 @@ public class RPiI2CNative {
      * @param address      the address of the device on the i2c bus
      * @param localAddress the local register address within the device to read from
      * @param offset       the offset within the buffer to start the operation. The actual byte offset will be evaluated as offset * width
-     * @param width        the width in bytes for each read or write of the I2c device
      * @param readBuffer   an array to receive the read bytes of size width*readCount
      * @param readCount    the number of times to read 'width' bytes from the device
-     * @param mask         an array of length width that contains a bit mask that will be applied to every read from the device
+     * @param mask         contains a bit mask that will be applied to every read from the device
      * @return return a negative value if an error was encountered otherwise the number of bytes read
      */
-    public static native int read(int fd, int address, byte localAddress, int offset, int width, int readCount, byte[] readBuffer, byte[] mask);
+    public static native int read(int fd, int address, byte localAddress, int offset, int readCount, byte[] readBuffer, byte mask);
 }
 
--- rpiio/src/main/native/net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative.c
+++ rpiio/src/main/native/net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative.c
@@ -36,19 +36,13 @@
 
 // Structures used in the ioctl() calls
 
-union i2c_smbus_data
-{
-    uint8_t  byte ;
-    uint16_t word ;
-    uint8_t  block [I2C_SMBUS_BLOCK_MAX + 2] ;	// block [0] is used for length + one more for PEC
-};
 
 struct i2c_smbus_ioctl_data
 {
-    char read_write ;
-    uint8_t command ;
-    int size ;
-    union i2c_smbus_data *data ;
+    char read_write;
+    uint8_t command;
+    int size;
+    uint8_t *data;
 };
 
 JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_open(JNIEnv *env, jclass clazz, jstring deviceName, jint address) {
@@ -73,122 +67,171 @@ JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative
 
 
 JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_write__IIBBB(JNIEnv *env, jclass clazz, jint fd, jint devId, jbyte localAddress, jbyte value, jbyte mask) {
-    ssize_t returnValue = 1;
-        char values[2];
-        values[0] = localAddress;
-        if (mask != 0xFF) {
-            uint8_t currentData;
-            if ((returnValue = read(fd,&currentData,1))) {
-                values[1] = (value & mask) | (currentData & ~mask);
-            }
-        }
-        else {
-            values[1] = value;
-        }
-        returnValue = write(fd,values,2);
-    return returnValue;
+    uint8_t data;
+    struct i2c_smbus_ioctl_data args ;
+
+    args.command    = localAddress;
+    args.size       = 2;
+    args.data       = &data;
+    if (mask != 0xFF) {
+        args.read_write = I2C_SMBUS_READ;
+        ioctl(fd, I2C_SMBUS, &args);
+        data = (value & mask) | (data & ~mask);
+    }
+    else {
+        data = value ;
+    }
+    args.read_write = I2C_SMBUS_WRITE;
+    return ioctl(fd, I2C_SMBUS, &args);
 };
 
 JNIEXPORT jbyte JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_read__IIB(JNIEnv *env, jclass clazz, jint fd, jint devId, jbyte localAddress) {
-    ssize_t returnValue = 0;
-        if ((returnValue = write(fd,&localAddress,1))) {
-            uint8_t value;
-            if ((returnValue = read(fd,&value,1))) {
-                return value;
-            }
-        }
-    return returnValue;
+    uint8_t data;
+    struct i2c_smbus_ioctl_data args ;
+
+    args.command    = localAddress;
+    args.size       = 2;
+    args.data       = &data;
+    args.read_write = I2C_SMBUS_READ;
+    ioctl(fd, I2C_SMBUS, &args);
+    return data;
 };
 
 JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_write__IIBB(JNIEnv *env, jclass clazz, jint fd, jint devId,  jbyte value , jbyte mask) {
-    ssize_t returnValue = 0;
-        if (mask != 0xFF) {
-            uint8_t currentData;
-            if ((returnValue = read(fd,&currentData,1))) {
-                value = (value & mask) | (currentData & ~mask);
-            }
-        }
-        else {
-            returnValue = write(fd,&value,1);
-        }
-    return returnValue;
+    uint8_t data ;
+    struct i2c_smbus_ioctl_data args ;
+
+    args.size = 1;
+    if (mask != 0xFF) {
+        args.command    = 0;
+        args.data = &data;
+        args.read_write = I2C_SMBUS_READ;
+        ioctl(fd, I2C_SMBUS, &args);
+        args.command = (value & mask) | (data & ~mask);
+    }
+    else {
+        args.command = value;
+    }
+    args.data = NULL;
+    args.read_write = I2C_SMBUS_WRITE;
+    return ioctl(fd, I2C_SMBUS, &args);
 };
 
 JNIEXPORT jbyte JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_read__II(JNIEnv *env, jclass clazz, jint fd, jint devId ) {
-    ssize_t returnValue = 0;
-        uint8_t value;
-        if ((returnValue = read(fd,&value,1))) {
-            return value;
-        }
-    return returnValue;
+    uint8_t data ;
+    struct i2c_smbus_ioctl_data args ;
+
+    args.read_write = I2C_SMBUS_READ;
+    args.command    = 0;
+    args.size       = 1;
+    args.data       = &data;
+    ioctl(fd, I2C_SMBUS, &args);
+    return data;
 };
 
-JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_write__IIIII_3BB(JNIEnv *env, jclass clazz, jint fd, jint devId, jint offset, jint width, jint writeCount, jbyteArray data, jbyteArray mask) {
-    ssize_t returnValue = 1;
-    uint8_t dataBlock[width];
+JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_write__IIII_3BB(JNIEnv *env, jclass clazz, jint fd, jint devId, jint offset, jint writeCount, jbyteArray data, jbyte mask) {
+    struct i2c_smbus_ioctl_data args ;
     jbyte *body = (*env)->GetPrimitiveArrayCritical(env, data, 0);
-    int i;
-    int ni;
 
-    if (mask != NULL) {
-        jbyte *maskBody = (*env)->GetPrimitiveArrayCritical(env, mask, 0);
-        uint8_t currentData[width];
-        if ((returnValue = read(fd,currentData,width))) {
-            for (i = 0; i < writeCount; ++i) {
-                for (ni = 0; ni < width; ++ni) {
-                    dataBlock[ni] = (body[(i*width)+ni+(offset*width)] & maskBody[ni]) | (currentData[ni] & ~maskBody[ni]);
-                }
-                returnValue = write(fd,dataBlock,width);
-            }
+    args.size       = 1;
+    if (mask != 0xFF) {
+        uint8_t data;
+        args.command = 0;
+        args.data = &data;
+        args.read_write = I2C_SMBUS_READ;
+        ioctl(fd, I2C_SMBUS, &args);
+        args.data = NULL;
+        args.read_write = I2C_SMBUS_WRITE;
+        for (i = 0; i < writeCount; ++i) {
+            args.command = (body[i+offset] & mask) | (data & ~mask);
+            ioctl(fd, I2C_SMBUS, &args);
         }
-        (*env)->ReleasePrimitiveArrayCritical(env, mask, maskBody, 0);
     }
     else {
-        returnValue = write(fd,body+offset,writeCount);
+        args.data = NULL;
+        args.read_write = I2C_SMBUS_WRITE;
+        for (i = 0; i < writeCount; ++i) {
+            args.command = body[i+offset];
+            ioctl(fd, I2C_SMBUS, &args);
+        }
     }
     (*env)->ReleasePrimitiveArrayCritical(env, data, body, 0);
-    return returnValue;
+    return 1;
 };
 
 JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_write__IIBIII_3B_3B(JNIEnv *env, jclass clazz, jint fd, jint devId, jbyte localAddress, jint offset, jint width, jint writeCount, jbyteArray data, jbyteArray mask) {
-
-    ssize_t returnValue = 1;
-    uint8_t dataBlock[width+1];
-    dataBlock[0] = localAddress;
-    int i;
-    int ni;
-
+    struct i2c_smbus_ioctl_data args ;
     jbyte *body = (*env)->GetPrimitiveArrayCritical(env, data, 0);
-    if (mask != NULL) {
+
+    args.command = localAddress;
+    args.size    = width+1;
+    if (mask != 0xFF) {
+        uint8_t dataBlock[width];
         jbyte *maskBody = (*env)->GetPrimitiveArrayCritical(env, mask, 0);
         uint8_t currentData[width];
-        if ((returnValue = write(fd,&localAddress,1)) && (returnValue = read(fd,&currentData,width))) {
-            for (i = 0; i < writeCount; ++i) {
-                for (ni = 0; ni < width; ++ni) {
-                    dataBlock[ni+1] = (body[(i*width)+ni+(offset*width)] & maskBody[ni]) | (currentData[ni] & ~maskBody[ni]);
-                }
-                returnValue = write(fd,dataBlock,width+1);
+        args.data = &currentData;
+        args.read_write = I2C_SMBUS_READ;
+        ioctl(fd, I2C_SMBUS, &args);
+        args.data = &dataBlock;
+        args.read_write = I2C_SMBUS_WRITE;
+        for (i = 0; i < writeCount; ++i) {
+            for (ni = 0; ni < width; ++ni) {
+                dataBlock[ni] = (body[(i*width)+ni+(offset*width)] & maskBody[ni]) | (currentData[ni] & ~maskBody[ni]);
             }
+            ioctl(fd, I2C_SMBUS, &args);
         }
         (*env)->ReleasePrimitiveArrayCritical(env, mask, maskBody, 0);
     }
     else {
+        args.read_write = I2C_SMBUS_WRITE;
         for (i = 0; i < writeCount; ++i) {
-            memcpy(&dataBlock+1,body+(i*width)+(offset*width),width);
-            returnValue = write(fd,&dataBlock,width+1);
+            args.data = body + (i*width) + (offset*width)]
+            ioctl(fd, I2C_SMBUS, &args);
         }
     }
     (*env)->ReleasePrimitiveArrayCritical(env, data, body, 0);
-    return returnValue;
-};
+    return 1;
+}
 
-JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_read__IIIII_3BB
-  (JNIEnv *env, jclass clazz, jint fd, jint devId, jint offset, jint width, jint readCount, jbyteArray data, jbyteArray mask) {
-  return 0;
+JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_read__IIII_3BB
+  (JNIEnv *env, jclass clazz, jint fd, jint devId, jint offset, jint readCount, jbyteArray data, jbyte mask) {
+    struct i2c_smbus_ioctl_data args ;
+    jbyte *body = (*env)->GetPrimitiveArrayCritical(env, data, 0);
+    uint8_t data ;
+    args.read_write = I2C_SMBUS_READ;
+    args.command    = 0;
+    args.size       = 1;
+    args.data       = &data;
+    for (i = 0; i < writeCount; ++i) {
+        ioctl(fd, I2C_SMBUS, &args);
+        body[(i*width)+(offset*width)] = data & mask;
+    }
+    (*env)->ReleasePrimitiveArrayCritical(env, data, body, 0);
+    return 1;
 };
 
 JNIEXPORT jint JNICALL Java_net_audumla_deviceaccess_i2cbus_rpi_jni_RPiI2CNative_read__IIBIII_3B_3B
   (JNIEnv *env, jclass clazz, jint fd, jint devId, jbyte localAddress, jint offset, jint width, jint readCount, jbyteArray data, jbyteArray mask) {
-  return 0;
+    struct i2c_smbus_ioctl_data args ;
+    jbyte *body = (*env)->GetPrimitiveArrayCritical(env, data, 0);
+    args.read_write = I2C_SMBUS_READ;
+    args.command    = localAddress;
+    args.size       = width+1;
+    for (i = 0; i < writeCount; ++i) {
+        args.data = body + (i*width)+(offset*width);
+        ioctl(fd, I2C_SMBUS, &args);
+    }
+    if (mask != NULL) {
+        jbyte *maskBody = (*env)->GetPrimitiveArrayCritical(env, mask, 0);
+        for (i = 0; i < writeCount; ++i) {
+            for (ni = 0; ni < width; ++i) {
+                int index = (i*width)+ni+(offset*width);
+                body[index] = body[index] & maskBody[ni];
+            }
+        }
+        (*env)->ReleasePrimitiveArrayCritical(env, mask, maskBody, 0);
+    }
+    (*env)->ReleasePrimitiveArrayCritical(env, data, body, 0);
+    return 1;
 };
 
