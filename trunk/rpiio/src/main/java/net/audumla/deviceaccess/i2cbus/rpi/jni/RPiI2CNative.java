7c10d987ad833bc3b57098118113a06cc1aa3d5b
audumla <audumla@31ee6a52-772c-232c-5b59-e78ef37cb892>
2014-02-24 12:18:53 +0000

    RPi I2C implementation

--- trunk/rpiio/src/main/java/net/audumla/deviceaccess/i2cbus/rpi/jni/RPiI2CNative.java
+++ trunk/rpiio/src/main/java/net/audumla/deviceaccess/i2cbus/rpi/jni/RPiI2CNative.java
@@ -26,7 +26,7 @@ public class RPiI2CNative {
 
     static {
         // Load the platform library
-        LibraryLoader.load("audumlaRPiI2C", "audumlaRPiI2C_ioctl.so");
+        LibraryLoader.load("audumlaRPiI2C", "audumlaRPiI2C_bcm2835.so");
     }
 
     /**
