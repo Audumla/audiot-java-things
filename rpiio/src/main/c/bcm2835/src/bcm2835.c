8f1f48c8dea0e0ce3077c84db9ff8312b569da9f
audumla <audumla@31ee6a52-772c-232c-5b59-e78ef37cb892>
2014-02-25 11:53:10 +0000

    RPi I2C implementation

--- rpiio/src/main/c/bcm2835/src/bcm2835.c
+++ rpiio/src/main/c/bcm2835/src/bcm2835.c
@@ -647,14 +647,14 @@ void bcm2835_i2c_set_baudrate(uint32_t baudrate, uint8_t bus)
 {
 	uint32_t divider;
 	// use 0xFFFE mask to limit a max value and round down any odd number
-	divider = (BCM2835_CORE_CLK_HZ / baudrate);
+	divider = (BCM2835_CORE_CLK_HZ / baudrate) & 0xFFFE;
 	bcm2835_i2c_setClockDivider( bus, (uint16_t)divider );
 }
 
 // \return the baudrate
 uint32_t bcm2835_i2c_get_baudrate(uint8_t bus) {
     volatile uint32_t* paddr = bcm2835_bsc[bus].paddr + BCM2835_BSC_DIV/4;
-    uint16_t divider = bcm2835_peri_read(paddr);
+    uint16_t divider = bcm2835_peri_read(paddr)  & 0xFFFE;
     return BCM2835_CORE_CLK_HZ / divider;
 }
 
