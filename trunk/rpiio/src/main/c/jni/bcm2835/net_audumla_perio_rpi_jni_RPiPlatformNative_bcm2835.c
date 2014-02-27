9bf148132ef317ced73eefe21043b4e9840956b8
audumla <audumla@31ee6a52-772c-232c-5b59-e78ef37cb892>
2014-02-27 11:51:35 +0000

    Created RPiPlatform class

---
+++ trunk/rpiio/src/main/c/jni/bcm2835/net_audumla_perio_rpi_jni_RPiPlatformNative_bcm2835.c
@@ -0,0 +1,23 @@
+#include <jni.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include "net_audumla_perio_i2cbus_rpi_jni_RPiPlatformNative.h"
+#include "bcm2835.h"
+
+JNIEXPORT jint JNICALL Java_net_audumla_perio_rpi_jni_RPiPlatformNative_init(JNIEnv *env, jclass clazz) {
+    return bcm2835_init();
+}
+
+JNIEXPORT void JNICALL Java_net_audumla_perio_rpi_jni_RPiPlatformNative_shutdown(JNIEnv *env, jclass clazz) {
+    bcm2835_close();
+}
+
+JNIEXPORT jint JNICALL Java_net_audumla_perio_rpi_jni_RPiPlatformNative_getRevision(JNIEnv *env, jclass clazz) {
+    return getHardwareRevision();
+}
